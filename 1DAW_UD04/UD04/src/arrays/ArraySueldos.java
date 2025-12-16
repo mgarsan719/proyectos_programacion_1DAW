cd870fcaa5e5bea92197fc49619c40044e2e05a7
mgarsan719 <margagon154@gmail.com>
2025-12-16 14:45:18 +0100

    nuevas clases

---
+++ 1DAW_UD04/UD04/src/arrays/ArraySueldos.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ArraySueldos
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		double[] sueldos = new double[5];
+
+		Arrays.fill(sueldos, 1234.56);
+
+		System.out.println(Arrays.toString(sueldos));
+
+		// Ecritura en el array
+		for (int i = 0; i < sueldos.length; i++)
+		{
+			sueldos[i] += sueldos[i] * 0.2;
+		}
+
+		System.out.println(Arrays.toString(sueldos));
+		
+		//Busqueda en el array
+		for(double e : sueldos) 
+		{
+			System.out.println(e);
+		}
+	}
+
+}
---
+++ 1DAW_UD04/UD04/src/arrays/ArraysDiferentesTipos.java
@@ -0,0 +1,41 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ArraysDiferentesTipos
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		int[] enteros = new int[5];
+
+		enteros[0] = 1;
+		enteros[1] = 2;
+		enteros[2] = 3;
+		enteros[3] = 4;
+		enteros[4] = 5;
+
+		double[] decimales = new double[5];
+		
+		decimales[0] = 1.1;
+		decimales[1] = 1.2;
+		decimales[2] = 1.3;
+		decimales[3] = 1.4;
+		decimales[4] = 1.5;
+
+		boolean[] booleanos = new boolean[5];
+		
+		booleanos[0] = true;
+		booleanos[1] = false;
+		booleanos[2] = true;
+		booleanos[3] = false;
+		booleanos[4] = true;
+
+		System.out.println(Arrays.toString(enteros));
+		System.out.println(Arrays.toString(decimales));
+		System.out.println(Arrays.toString(booleanos));
+	}
+
+}
