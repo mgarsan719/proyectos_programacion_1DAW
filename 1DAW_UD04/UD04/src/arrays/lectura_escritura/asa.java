4bef702e24270eed938db0a7448c64388a30c472
mgarsan719 <margagon154@gmail.com>
2026-02-10 13:58:47 +0100

    examen arrays

---
+++ 1DAW_UD04/UD04/src/arrays/lectura_escritura/asa.java
@@ -0,0 +1,28 @@
+package arrays.lectura_escritura;
+
+public class asa
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		int[] a, b;
+		
+		a = new int[2];
+		
+		b=a;
+		
+		b[0]=5;
+		
+		System.out.println(b[0]);
+		
+		System.out.println(a[0]);
+
+		
+		
+		
+		
+	}
+
+}
